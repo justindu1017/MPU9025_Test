e762f1e7763f6a919c63b07faba4c7bc057d2adf
Justin Du <justinduthebest@gmail.com>
2021-12-09 02:25:28 +0800

    finish CSV

---
+++ writeCSV.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+load_dotenv() 
+def writeToCSV(mpu6050_ACCEL_vec, mpu6050_GYRO_vec, AK8963_vec):
+
+    fileRoute = os.getenv("path_csv")
+    dataArr = []
+    for z in list(zip(mpu6050_ACCEL_vec, mpu6050_GYRO_vec, AK8963_vec)):
+        subArr = []
+        for i in z:
+            subArr += i
+        dataArr.append(subArr)
+    data = pd.DataFrame(dataArr, columns=['Accel_X', 'Accel_Y', 'Accel_Z', 'GYRO_X', 'GYRO_Y', 'GYRO_Z', 'Magn_X', 'Magn_Y', 'Magn_Z'])
+    data.to_csv(fileRoute, index=False)
+
+
+#     import pandas as pd
+# cities = pd.DataFrame([['Sacramento', 'California'], ['Miami', 'Florida']], columns=['City', 'State'])
+# cities.to_csv('cities.csv', index=False)
+
+
